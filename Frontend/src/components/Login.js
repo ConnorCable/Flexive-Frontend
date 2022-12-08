e71b235fea14b77170a766fabb31d7c1061a3c01
ctcmc <ccab0528@gmail.com>
2022-12-08 07:03:43 -0800

    testing jwt

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -36,12 +36,8 @@ const Login = () => {
       })
         .then((response) => Promise.all([response.json(), response.headers]))
         .then(([body, headers]) => {
-          setJwt(headers.get("authorization"), setData(body))
-
+          console.log(headers.get("authorization"))
         })
-      }
-      else if (jwt && data){
-        navigate('/profile')
       }
      
 
